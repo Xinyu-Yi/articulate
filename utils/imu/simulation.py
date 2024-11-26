4daa5ea01790819fe6c6989be26a6f2bd9823420
yxy <yixy20@mails.tsinghua.edu.cn>
2024-11-26 22:22:04 +0800

    update simulation

--- utils/imu/simulation.py
+++ utils/imu/simulation.py
@@ -19,8 +19,8 @@ class IMUSimulator:
         r"""
         Set the 6DoF trajectory for the IMU.
 
-        :param pWS: IMU position trajectory in the world frame in shape [N, 3].
-        :param RWS: IMU orientation trajectory in the world frame in shape [N, 3, 3].
+        :param pWS: IMU position trajectory in the world frame in shape [..., 3].
+        :param RWS: IMU orientation trajectory in the world frame in shape [..., 3, 3].
         :param fps: Frame rate.
         """
         self.pWS = pWS
@@ -32,28 +32,28 @@ class IMUSimulator:
         Get the sensor-local acceleration.
 
         :param gW: Gravity acceleration in the world frame.
-        :return: Acceleration in shape [N, 3].
+        :return: Acceleration in shape [..., 3].
         """
         a = self.pWS[:-2] - 2 * self.pWS[1:-1] + self.pWS[2:]
         a0 = 2 * self.pWS[0] - 5 * self.pWS[1] + 4 * self.pWS[2] - self.pWS[3]
         a1 = 2 * self.pWS[-1] - 5 * self.pWS[-2] + 4 * self.pWS[-3] - self.pWS[-4]
-        aW = torch.cat((a0.unsqueeze(0), a, a1.unsqueeze(0))) * self.fps * self.fps
-        aS = self.RWS.transpose(1, 2).matmul((aW - torch.tensor(gW, device=aW.device)).unsqueeze(-1)).squeeze(-1)
+        aW = torch.cat((a0.unsqueeze(0), a, a1.unsqueeze(0)), dim=0) * self.fps * self.fps
+        aS = self.RWS.transpose(-1, -2).matmul((aW - torch.tensor(gW, device=aW.device)).unsqueeze(-1)).squeeze(-1)
         return aS
 
     def get_angular_velocity(self):
         r"""
         Get the sensor-local angular velocity.
 
-        :return: Angular velocity in shape [N, 3].
+        :return: Angular velocity in shape [..., 3].
         """
         Rdot = self.RWS[2:] - self.RWS[:-2]
         Rdot0 = -3 * self.RWS[0] + 4 * self.RWS[1] - self.RWS[2]
         Rdot1 = 3 * self.RWS[-1] - 4 * self.RWS[-2] + self.RWS[-3]
         Rdot = torch.cat((Rdot0.unsqueeze(0), Rdot, Rdot1.unsqueeze(0)), dim=0) * self.fps / 2
-        wShat = self.RWS.transpose(1, 2).bmm(Rdot)
-        wShat = (wShat - wShat.transpose(1, 2)) / 2
-        wS = torch.stack((wShat[:, 2, 1], wShat[:, 0, 2], wShat[:, 1, 0]), dim=1)
+        wShat = self.RWS.transpose(-1, -2).matmul(Rdot)
+        wShat = (wShat - wShat.transpose(-1, -2)) / 2
+        wS = torch.stack((wShat[..., 2, 1], wShat[..., 0, 2], wShat[..., 1, 0]), dim=-1)
         return wS
 
     def get_magnetic_field(self, mW=(1., 0, 0)):
@@ -61,7 +61,7 @@ class IMUSimulator:
         Get the sensor-local magnetic field.
 
         :param mW: Magnetic field in the world frame.
-        :return: Magnetic field in shape [N, 3].
+        :return: Magnetic field in shape [..., 3].
         """
-        mS = self.RWS.transpose(1, 2).matmul(torch.tensor(mW).unsqueeze(-1)).squeeze(-1)
+        mS = self.RWS.transpose(-1, -2).matmul(torch.tensor(mW, device=self.RWS.device).unsqueeze(-1)).squeeze(-1)
         return mS
